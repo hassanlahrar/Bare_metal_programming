b0a2f802b7dfca9a7f569373c12fd5100d2ae1b5
hassanlahrar <32880039+hassanlahrar@users.noreply.github.com>
2023-05-13 11:29:13 +0200

    Add files via upload

---
+++ stm32_bareMetal_template/main/main.c
@@ -0,0 +1,86 @@
+#include <unistd.h>
+#include <system_stm32f1xx.h>
+#include <stm32f1xx.h>
+
+void SystemClockConfig(void);
+static void init_delay_timer(void);
+void TIM2_IRQHandler(void);
+
+int main(void) {
+	
+	SystemClockConfig();
+	init_delay_timer();
+	TIM2_IRQHandler();
+	
+	while (1){
+	   __asm("nop");
+	}
+    return 0;
+}
+
+void SystemClockConfig(void){
+	//Activation HSI (HSION)
+	RCC->CR &= 0;
+	RCC->CR |= 1;
+	//Attendre l'activation de HSI (HSIRDY)
+	while( !(RCC->CR & (1<<1)) ){};
+		
+	//PLL prendre un facteur 9
+	RCC->CFGR |= (0b0111 << 18);
+	//Activer le PLL -> PLLON
+	RCC->CR |= (1 << 24);
+	//Attendre l'activation de PLL ->PLLRDY
+	while( !(RCC->CR & (1<<25)) ){};
+		
+	//Mettre PLL comme clock source /SW
+	RCC->CFGR &= ~0b11;
+	RCC->CFGR |= 0b10;
+	//Attendre le changement de clock source /SWS
+	while(!(RCC->CFGR & (0b10<<2))){};
+		
+	//Prescaler 2 pour APB1
+	RCC->CFGR &= ~(0b111 << 8);
+	RCC->CFGR |= (0b100 << 8);
+	
+	/* RCC */
+	RCC->APB2ENR |= (1 << 4) | (1 << 2); // Enable RCC for GPIOC, GPIOA Pin/Port
+	
+	/* MODER */
+	GPIOA->CRL &= ~((1 << 22) | (1 << 23) | (1 << 21) | (1 << 20)); // Clear GPIO PA5 MODE
+	
+	/* OUTPUT */
+	GPIOA->CRL |= (1 << 21); // Set GPIO PA5 MODE (01 : output)
+	
+}
+
+static void init_delay_timer(void)
+{
+	    /*Activer le clock pour TIM2*/
+	    RCC->APB1ENR &= 0 << 0;
+	     RCC->APB1ENR |= 1 << 0;
+
+	    /*Choisir le prescaler*/
+	    TIM2->PSC = 35999; /*diviser la frequence source par 36000*/
+
+	    /*Choisir la valeur de compteur*/
+	    TIM2->ARR = 99; /*interruption tous les 1000ms*/
+
+	    /*mise a jour des valeurs psc, arr */
+	    TIM2->EGR |= 1;
+	    
+	    TIM2->DIER |= 1;
+
+            NVIC_EnableIRQ(TIM2_IRQn);
+	    
+	    /*start le timer*/
+	    TIM2->CR1 |= 1;
+}
+
+void TIM2_IRQHandler(void){
+   if ( TIM2->SR  & 1)
+   {
+    TIM2->SR &= ~1;
+    GPIOA->ODR ^= (1 << 5);
+		 }   
+    
+}
